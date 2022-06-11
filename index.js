98f2d87d34860af2430c760ac59ff4bba40f832b
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-06-11 13:30:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ app.get('/', (req, res) =>{
 })
 
 // database MYSQL
-connection
-    .authenticate()
-    .then(() => {
-        console.log('Banco de Dados conectado com SUCESSO!')
-    }).catch((error) =>{
-        console.log(error);
-    })
+
 
 app.listen(8080,() =>{
     console.log('SERVER START !');
